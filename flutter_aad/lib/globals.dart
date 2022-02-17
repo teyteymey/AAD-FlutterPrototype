dbff025c7ed823d7b6235988dab54d28274ee525
teyteymey <tatiana.meyer.u@gmail.com>
2022-02-18 00:08:51 +0100

    changes in variables

--- flutter_aad/lib/globals.dart
+++ flutter_aad/lib/globals.dart
@@ -2,10 +2,17 @@ library my_prj.globals;
 
 import 'package:contacts_service/contacts_service.dart';
 
-List<List<String>> partyInfo = [
-  ['House Party', 'Deventer', '20 Feb, 20:00h', '2.jpg'],
-  ['Disco', 'Deventer', '20 Feb, 20:00h', '3.jpg'],
-  ['Costume Party', 'Den Heks, Deventer', '15 Feb, 17:00h', '1.jpg'],
+List<List<List<String>>> partyInfo = [
+  [
+    ['House Party', 'Deventer', '20 Feb, 20:00h', '2.jpg'],
+    []
+  ],
+  [
+    ['Disco', 'Deventer', '20 Feb, 20:00h', '3.jpg'],
+    []
+  ],
+  [
+    ['Costume Party', 'Den Heks, Deventer', '15 Feb, 17:00h', '1.jpg'],
+    []
+  ]
 ];
-
-List<Contact> contacts = [];
